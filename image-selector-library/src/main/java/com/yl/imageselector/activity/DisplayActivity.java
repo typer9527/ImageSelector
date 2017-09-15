2c93760210d5dc3aac68f51e0ac22aeb5dbdbcec
Yangli <yangl7240@gmail.com>
2017-09-15 16:21:16 +0800

    重新构建项目

--- image-selector-library/src/main/java/com/yl/imageselector/activity/DisplayActivity.java
+++ image-selector-library/src/main/java/com/yl/imageselector/activity/DisplayActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 
 import com.yl.imageselector.R;
 import com.yl.imageselector.view.ZoomImageView;
